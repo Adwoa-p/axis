f8cdfdad493da1f9504aa9a2b8a620aa22922f86
nutifafa <attorfafa@gmail.com>
2025-12-11 10:02:30 +0000

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-500'>
+        <Button
+          size="lg"
+        >
+          Hello
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
